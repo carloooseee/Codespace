a89d38ddf0c221f693e5f02eaf6d850f684bcd44
Carlos <carloooseeen@gmail.com>
2024-03-05 22:32:58 +0800

    Activity

--- S9_Activity/main.js
+++ S9_Activity/main.js
@@ -5,14 +5,16 @@
 //     }
 //     console.log(" ");
 // };
-function generatePyramid() {
-    var totalNumberofRows = 5;
-    var arr = new Array();
-    for (var i = 1; i <= totalNumberofRows; i++) {
-      for (var j = 1; j <= i; j++) {
-        arr.push(j);
-        console.log(j);
-      }
-      console.log("\n");
-    }
-  }
+
+
+// function generatePyramid() {
+//     var totalNumberofRows = 5;
+//     var arr = new Array();
+//     for (var i = 1; i <= totalNumberofRows; i++) {
+//       for (var j = 1; j <= i; j++) {
+//         arr.push(j);
+//         console.log(j);
+//       }
+//       console.log("\n");
+//     }
+//   }
